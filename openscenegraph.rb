f36520f37581071bc63dd8efa65469371b21ad4b
Martin Aumüller <aumuell@reserv.at>
2015-05-07 23:24:08 +0200

    patch for not overriding default c++ library

--- openscenegraph.rb
+++ openscenegraph.rb
@@ -4,6 +4,7 @@ class Openscenegraph < Formula
   homepage 'http://www.openscenegraph.org'
   url 'http://www.openscenegraph.org/downloads/developer_releases/OpenSceneGraph-3.2.1.zip'
   sha1 '5c666531f7d487075fd692d89f1e05036306192a'
+  patch :p0, :DATA
 
   depends_on 'cmake' => :build
 
@@ -22,3 +23,19 @@ class Openscenegraph < Formula
     system "false"
   end
 end
+
+__END__
+--- CMakeLists.txt.dist	2015-05-07 09:22:27.000000000 +0200
++++ CMakeLists.txt	2015-05-07 09:23:01.000000000 +0200
+@@ -836,10 +836,7 @@
+         # remain unset.
+ 
+         IF (APPLE)
+-            # set standard lib, clang defaults to c++0x
+-            set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++98")
+-            set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libstdc++")
+-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++98 -stdlib=libstdc++ -Wno-overloaded-virtual -Wno-conversion")
++            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-overloaded-virtual -Wno-conversion")
+             set(WARNING_CFLAGS "")
+         ENDIF()
+ ENDIF()
