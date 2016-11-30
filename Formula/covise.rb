5260c70dc6a096a32374ae554b82b57d26315108
Martin Aumüller <aumuell@reserv.at>
2016-11-30 02:39:01 +0100

    pull in fixes
    - install required config files
    - update to version w/o visionaray viewer
    - don't break build aginst vtk 7.1

--- Formula/covise.rb
+++ Formula/covise.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Covise < Formula
   homepage 'https://www.hlrs.de/covise/'
   desc 'Visualization environment for scientific and engineering data'
-  url 'https://github.com/hlrs-vis/covise.git', :using => :git, :revision => '57f072a3e72005e795af4e0d7191387c3a80ad64'
+  url 'https://github.com/hlrs-vis/covise.git', :using => :git, :revision => '02fa062fc7c7d97e6e3c807b79e9f2a96aee60c4'
   version '2016.11'
   head 'https://github.com/hlrs-vis/covise.git'
 
