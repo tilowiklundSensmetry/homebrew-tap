141d66ddb72b22ae8516b34cc42bbcce20dd4b4c
Martin Aumüller <aumuell@reserv.at>
2017-06-09 16:49:23 +0200

    make build more reliable

--- Formula/covise.rb
+++ Formula/covise.rb
@@ -7,9 +7,7 @@ class Covise < Formula
   version '2017.6'
   head 'https://github.com/hlrs-vis/covise.git'
 
-  option "with-gdcm", "Build with GDCM for DICOM reading"
   option "with-cuda", "Build with CUDA support"
-  option "with-qt4", "Build against Qt 4 instead of Qt 5"
   option "with-jpeg", "Build against libjpeg instead of libjpeg-turbo"
   option "with-x11", "Build against X11 and Open Motif"
   option "without-cover", "Build without OpenCOVER VR renderer"
@@ -28,21 +26,26 @@ class Covise < Formula
   depends_on "freetype"
   depends_on "boost"
   depends_on "python3"
-  depends_on "homebrew/science/gdcm" if build.with? "gdcm"
-  depends_on :fortran => :optional
+  depends_on "qt"
   depends_on :x11 => :optional
   depends_on "homebrew/x11/openmotif" if build.with? "x11"
   depends_on "hlrs-vis/tap/openinventor" if build.with? "x11"
-  depends_on "qt" if build.with? "qt4"
-  depends_on "qt5" if build.without? "qt4"
   depends_on "assimp" => :optional
-  depends_on "vtk" => :optional
   depends_on "cgns" => :optional
   depends_on "snappy" => :optional
   depends_on "Caskroom/cask/cuda" if build.with? "cuda"
 
   depends_on "openscenegraph" if build.with? "cover"
 
+  depends_on :fortran  => :optional
+  #conflicts_with "fortran", :because => "linking with Fortran libraries fails without explicit Fortran dependency, specify --with-fortran" if build.without? "fortran"
+
+  depends_on "vtk" => :optional
+  conflicts_with "vtk", :because => "including VTK headers fails without explicit VTK dependency, specify --with-vtk" if build.without? "vtk"
+
+  option "with-gdcm", "Build with GDCM for DICOM reading"
+  depends_on "homebrew/science/gdcm" if build.with? "gdcm"
+  conflicts_with "gdcm", :because => "including GDCM headers fails without explicit GDCM dependency, specify --with-gdcm" if build.without? "gdcm"
 
   def install
     ENV["COVISEDIR"] = buildpath
@@ -54,6 +57,9 @@ class Covise < Formula
     end
 
     cmake_args = std_cmake_args
+    cmake_args << "-DCOVISE_USE_VISIONARAY=OFF"
+    cmake_args << "-DCOVISE_USE_FORTRAN:BOOL=ON" if build.with? "fortran"
+    cmake_args << "-DCOVISE_USE_FORTRAN:BOOL=OFF" if build.without? "fortran"
     cmake_args << "-DCOVISE_USE_CUDA:BOOL=ON" if build.with? "cuda"
     cmake_args << "-DCOVISE_USE_CUDA:BOOL=OFF" if build.without? "cuda"
     cmake_args << "-DCOVISE_USE_X11:BOOL=ON" if build.with? "x11"
@@ -61,14 +67,6 @@ class Covise < Formula
 
     #cmake_args << "-DCOVISE_USE_VIRVO=OFF"
     #cmake_args << "-DCOVISE_BUILD_MODULES=OFF"
-
-    if build.with? "qt4"
-        cmake_args << "-DCOVISE_USE_QT4=ON"
-        #cmake_args << "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_prefix}"
-    else
-        cmake_args << "-DCOVISE_USE_QT4=OFF"
-        #cmake_args << "-DCMAKE_PREFIX_PATH=#{Formula["qt5"].opt_prefix}"
-    end
 
     mkdir "build.covise" do
         system "cmake", "..", *cmake_args
