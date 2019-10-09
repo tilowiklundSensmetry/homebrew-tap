2eff07a6d6fb36baf79feccd2808affa36ce2a16
Martin Aumüller <aumuell@reserv.at>
2019-10-09 10:59:28 +0200

    don't require unittest++

--- Formula/cal3d.rb
+++ Formula/cal3d.rb
@@ -2,18 +2,20 @@ class Cal3d < Formula
   desc "Skeletal 3D character animation library written in C++"
   homepage "http://cal3d.sourceforge.net/docs/api/html/cal3dfaq.html"
   url 'https://github.com/hlrs-vis/cal3d.git', :using => :git, :revision => '8cbf44f8c20a191b67063cb943420ec55e7125b8'
-  version '2018.5.1'
+  version '2018.5.2'
   head "git://github.com/hlrs-vis/cal3d.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
     Dir.chdir('cal3d')
     system "autoreconf", "-i"
     # ENV.deparallelize  # if your formula fails when building in parallel
     system "./configure", "--disable-debug",
+                          "--disable-unittest",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
