913b8f5c4198a9483f2bcfa3096fc76900d14a50
Martin Aumüller <aumuell@reserv.at>
2020-08-06 13:35:53 +0200

    update download url

--- Formula/opencrg.rb
+++ Formula/opencrg.rb
@@ -1,7 +1,7 @@
 class Opencrg < Formula
   desc "Creation and evaluation of road surfaces"
   homepage "http://www.opencrg.org/"
-  url "http://www.vires.com/opencrg/tools/OpenCRG.1.1.2.zip"
+  url "http://www.opencrg.org/tools/OpenCRG.1.1.2.zip"
   version "1.1.2"
   sha256 "3ff63ced6190dc22278cd74592e165aca9acf93153c51c93c569744d7714c97b"
   # depends_on "cmake" => :build
