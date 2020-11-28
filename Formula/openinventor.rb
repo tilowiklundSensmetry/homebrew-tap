f9ca1e5f9485119db65233d04b7383d654b3b95c
Martin Aumüller <aumuell@reserv.at>
2020-11-28 21:03:43 +0100

    openinventor: use CMake build system

--- Formula/openinventor.rb
+++ Formula/openinventor.rb
@@ -1,27 +1,26 @@
-require 'formula'
-
 class Openinventor < Formula
   homepage 'http://oss.sgi.com/projects/inventor/'
-  url 'https://github.com/aumuell/open-inventor.git'
+  url 'https://github.com/aumuell/open-inventor.git', :using => :git
   version '2.1.5-10'
   sha256 ''
 
-  depends_on :x11
+  depends_on "cmake" => :build
+
   depends_on "openmotif"
-  depends_on "jpeg-turbo"
-  depends_on "giflib"
+  depends_on "libxi"
+  depends_on "mesa"
+  depends_on "mesa-glu"
+  depends_on "jpeg"
 
   def install
-    # Make ivman find the libraries it needs
-    ENV['DYLD_LIBRARY_PATH'] = "#{buildpath}/lib"
-
-    system "make",
-        "IVPREFIX=#{prefix}",
-        "BUILDMAN=1",
-        "install"
+    mkdir "brew-build" do
+      system "cmake", "..", *std_cmake_args,
+        "-DCMAKE_MACOSX_RPATH=FALSE"
+      system "make", "install"
+    end
   end
 
   test do
-    system "ivfix #{share}/inventor/data/models/moon.iv"
+    system "ivcat #{share}/inventor/data/models/moon.iv"
   end
 end
